8f6049a563b2c314bdc073abea3631fb9ed15680
iamyangqi@sina.com <iamyangqi@sina.com>
2018-02-11 22:46:50 +0800

    add convertLink

--- getTickets.php
+++ getTickets.php
@@ -1,7 +1,6 @@
 <?php
-    $url = $_GET["url"];
-    $pic = $_GET["pic"];
-    $title = $_GET["title"];
+    $q = $_GET["q"];
+    $page = $_GET["page"];
 
     include "TopSdk.php";
     date_default_timezone_set('Asia/Shanghai');
@@ -11,11 +10,13 @@
     $c->secretKey = "e952387d2329198293dd59d37afff45c";
     $c->format = 'json';
 
-    $req = new TbkTpwdCreateRequest;
-    $req->setUserId("2241216956");
-    $req->setText($title);
-    $req->setUrl($url);
-    $req->setLogo($pic);
+    $req = new TbkDgItemCouponGetRequest;
+    $req->setAdzoneId("242922082");
+    $req->setPlatform("1");
+//    $req->setCat("16,18");
+    $req->setPageSize("20");
+    $req->setQ($q);
+    $req->setPageNo($page);
 
     $resp = $c->execute($req);
     echo json_encode($resp);
